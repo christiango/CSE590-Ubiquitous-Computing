9a36e36a93b1b6212e2baf91ac3a004574e52e13
Christian Gonzalez <christianjordangonzalez@gmail.com>
2018-04-07 09:30:22 -0700

    Basic step tracker activity

--- StepTracker/app/src/main/java/go/christian/steptracker/MainActivity.java
+++ StepTracker/app/src/main/java/go/christian/steptracker/MainActivity.java
@@ -17,4 +17,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DiagnosticsActivity.class);
         startActivity(intent);
     }
+
+    public void launchStepCount(View view) {
+        Intent intent = new Intent(this, StepCountActivity.class);
+        startActivity(intent);
+    }
 }
