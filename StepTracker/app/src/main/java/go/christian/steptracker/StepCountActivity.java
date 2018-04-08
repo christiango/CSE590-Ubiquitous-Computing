0294d9c976390c65b7037bd0c475f9d3023dcb8e
Christian Gonzalez <christianjordangonzalez@gmail.com>
2018-04-07 21:08:57 -0700

    Remove hideous main menu

--- StepTracker/app/src/main/java/go/christian/steptracker/StepCountActivity.java
+++ StepTracker/app/src/main/java/go/christian/steptracker/StepCountActivity.java
@@ -1,45 +1,69 @@
 package go.christian.steptracker;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class StepCountActivity extends AppCompatActivity implements SensorEventListener {
 
-    private StepCounter _stepCounter = StepCounterFactory.GetStepCounter();
+  private StepCounter _stepCounter = StepCounterFactory.GetStepCounter();
 
-    private TextView _stepCountTextView;
+  private TextView _stepCountTextView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_step_count);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_step_count);
 
-        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        Sensor accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_UI);
+    SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    Sensor accelSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    sensorManager.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_UI);
 
-        _stepCountTextView = findViewById(R.id.stepCount);
+    _stepCountTextView = findViewById(R.id.stepCount);
 
-        ImageView image = findViewById(R.id.progressImage);
-        image.setImageDrawable(new PacmanDrawable());
+    ImageView image = findViewById(R.id.progressImage);
+    image.setImageDrawable(new PacmanDrawable());
+  }
+
+  @Override
+  public void onSensorChanged(SensorEvent event) {
+    if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+      _stepCountTextView.setText("" + _stepCounter.addData(event.values));
     }
+  }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            _stepCountTextView.setText("" + _stepCounter.addData(event.values));
-        }
+  @Override
+  public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.step_counter_settings, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle item selection
+    switch (item.getItemId()) {
+      case R.id.set_step_goals:
+        return true;
+      case R.id.navigate_diagnostics:
+        Intent intent = new Intent(this, DiagnosticsActivity.class);
+        startActivity(intent);
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
+  }
 }
